eb1846de230904fef8d29ab81b092ef4153dc9ef
Nathan Weizenbaum <nex342@gmail.com>
2008-12-09 04:00:41 -0800

    Add more comics.

--- scrape.rb
+++ scrape.rb
@@ -11,13 +11,19 @@ $LOAD_PATH << dir unless $LOAD_PATH.include?(dir)
 require 'entry'
 require 'feeds'
 
-feed(:frazz, 'http://comics.com/frazz',
-     :once => lambda { !(doc/'title').inner_text.include?('404') }) do
-  self.time = Time.parse((doc/'.STR_Date').first.inner_text)
-  el = (doc/'.STR_StripImage').first
-  self.link = el.attributes['href']
-  img((el/'img').first)
+def comics_dot_com(name)
+  feed(name, "http://comics.com/#{name}",
+       :once => lambda { !(doc/'title').inner_text.include?('404') }) do
+    self.time = Time.parse((doc/'.STR_Date').first.inner_text)
+    el = (doc/'.STR_StripImage').first
+    self.link = el.attributes['href']
+    img((el/'img').first)
+  end
 end
+
+comics_dot_com :frazz
+comics_dot_com :rose_is_rose
+comics_dot_com :get_fuzzy
 
 get '/:comic' do
   content_type 'application/atom+xml', :charset => 'utf-8'
