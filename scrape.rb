bb9a2c2de86d7ef5f309b47fe9af03989e130287
Nathan Weizenbaum <nex342@gmail.com>
2008-12-09 03:31:00 -0800

    Add first scrapee (Frazz).

--- scrape.rb
+++ scrape.rb
@@ -11,6 +11,14 @@ $LOAD_PATH << dir unless $LOAD_PATH.include?(dir)
 require 'entry'
 require 'feeds'
 
+feed(:frazz, 'http://comics.com/frazz',
+     :once => lambda { !(doc/'title').inner_text.include?('404') }) do
+  self.time = Time.parse((doc/'.STR_Date').first.inner_text)
+  el = (doc/'.STR_StripImage').first
+  self.link = el.attributes['href']
+  img((el/'img').first)
+end
+
 get '/:comic' do
   content_type 'application/atom+xml', :charset => 'utf-8'
   Feeds[params['comic']].render
