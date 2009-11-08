eadc5b9865ca20992c1987cb5f684bc211976a43
Nathan Weizenbaum <nex342@gmail.com>
2009-11-07 22:47:01 -0800

    Add authors for all of the feeds.

--- app.rb
+++ app.rb
@@ -10,8 +10,9 @@ require 'open-uri'
 require File.dirname(__FILE__) + '/lib/scrape'
 
 def comics_dot_com(name, options = {})
-  Scrape(name, "http://comics.com/#{options[:url] || name}",
-       :once => lambda { !(doc/'title').inner_text.include?('404') }) do
+  url = options.delete(:url) || name
+  opts = {:once => lambda { !(doc/'title').inner_text.include?('404') }}.merge(options)
+  Scrape(name, "http://comics.com/#{url}", opts) do
     self.time = Time.parse((doc/'.STR_Date').first.inner_text)
     el = (doc/'.STR_StripImage').first
     self.link = el.attributes['href']
@@ -20,27 +21,28 @@ def comics_dot_com(name, options = {})
 end
 
 def gocomics(name, options = {})
-  Scrape(name, "http://www.gocomics.com/#{options[:url] || name}/") do
+  url = options.delete(:url) || name
+  Scrape(name, "http://www.gocomics.com/#{url}/", options) do
     self.time = Time.parse((doc/'.feature-nav li').first.inner_text)
     self.link = (doc/'.feature h1 a').first.attributes['href']
     img((doc/'.feature_item img').first)
   end
 end
 
-comics_dot_com :frazz
-comics_dot_com :rose_is_rose
-comics_dot_com :get_fuzzy
-comics_dot_com :arlo_and_janis, :url => 'arlo&janis'
+comics_dot_com :frazz, :author => "Jef Mallett"
+comics_dot_com :rose_is_rose, :author => "Pat Brady"
+comics_dot_com :get_fuzzy, :author => "Darby Conley"
+comics_dot_com :arlo_and_janis, :url => "arlo&janis", :author => "Jimmy Johnson"
 
-gocomics :non_sequitur, :url => 'nonsequitur'
+gocomics :non_sequitur, :url => 'nonsequitur', :author => "Wiley"
 
-Scrape :dominic_deegan, 'http://dominic-deegan.com/' do
+Scrape :dominic_deegan, 'http://dominic-deegan.com/', :author => "Mookie" do
   self.time = Time.parse((doc/'.comic .date').first.inner_text)
   self.link = "/view.php?date=#{time.strftime('%Y-%m-%d')}"
   img((doc/'.comic img').first)
 end
 
-Scrape "8_bit_theater", 'http://www.nuklearpower.com/8-bit-theater/' do
+Scrape "8_bit_theater", 'http://www.nuklearpower.com/8-bit-theater/', :author => "Brian Clevinger" do
   prev_doc = Hpricot(open((doc/'.navbar-previous a').first.attributes['href']))
   self.link = (prev_doc/'.navbar-next a').first.attributes['href']
   self.time = Time.parse(self.link[/[0-9]+\/[0-9]+\/[0-9]+/])
@@ -50,7 +52,7 @@ Scrape "8_bit_theater", 'http://www.nuklearpower.com/8-bit-theater/' do
   img(img)
 end
 
-Scrape.undated :girls_with_slingshots, 'http://daniellecorsetto.com/gws.html' do
+Scrape.undated :girls_with_slingshots, 'http://daniellecorsetto.com/gws.html', :author => "Danielle Corsetto" do
   img = (doc/'#gwsblog img').first
   src = img.attributes['src']
   check_updated src
@@ -60,7 +62,7 @@ Scrape.undated :girls_with_slingshots, 'http://daniellecorsetto.com/gws.html' do
   (doc/'#gwsblog').html
 end
 
-Scrape.undated :elsie_hooper, 'http://www.elsiehooper.com/todaysserial.htm' do
+Scrape.undated :elsie_hooper, 'http://www.elsiehooper.com/todaysserial.htm', :author => "Robert D. Krzykowski" do
   ps = doc/'body > div.font > p'
   self.title = ps.first.inner_text
   img = (ps[1]/'img').first
