72741e109a6d09c82542a6bc7cf786e26c2f88c9
Nathan Weizenbaum <nex342@gmail.com>
2009-11-07 22:34:39 -0800

    Namespace Feed and Entry.

--- lib/scrape/entry.rb
+++ lib/scrape/entry.rb
@@ -3,22 +3,23 @@ require 'hpricot'
 require 'open-uri'
 require 'haml'
 
-class Entry
-  attr_accessor :feed, :url, :time, :link, :title, :text, :created_at
+module Scrape
+  class Entry
+    attr_accessor :feed, :url, :time, :link, :title, :text, :created_at
 
-  def initialize(feed)
-    self.feed = feed
-    self.created_at = Time.now
+    def initialize(feed)
+      self.feed = feed
+      self.created_at = Time.now
 
-    @doc = nil until instance_eval(&(feed.opts[:once] || proc {true}))
-    self.text ||= instance_eval(&feed.block)
-  rescue Exception => e
-    self.time = Time.now
-    self.text = "<h1>Error: #{h e.class}</h1><h2>#{h e.message}</h2>"
-  end
+      @doc = nil until instance_eval(&(feed.opts[:once] || proc {true}))
+      self.text ||= instance_eval(&feed.block)
+    rescue Exception => e
+      self.time = Time.now
+      self.text = "<h1>Error: #{h e.class}</h1><h2>#{h e.message}</h2>"
+    end
 
-  def render
-    Haml::Engine.new(<<HAML).render(self)
+    def render
+      Haml::Engine.new(<<HAML).render(self)
 %entry
   %id== \#{URL}/\#{feed.name}/\#{text.hash.to_s(16)}
   %title= title || time.strftime('%d %B %Y')
@@ -27,28 +28,29 @@ class Entry
   - if link
     %link{:rel => "alternate", :href => link}/
 HAML
-  end
+    end
 
-  private
+    private
 
-  def doc
-    times = 0
-    @doc ||= Hpricot(open(feed.url))
-  rescue Errno::ECONNREFUSED => e
-    times += 1
-    retry if times < 3
-    raise e
-  end
+    def doc
+      times = 0
+      @doc ||= Hpricot(open(feed.url))
+    rescue Errno::ECONNREFUSED => e
+      times += 1
+      retry if times < 3
+      raise e
+    end
 
-  def img(el)
-    "<img src='#{h el.attributes['src']}' alt='#{h el.attributes['alt'] || ''}'>"
-  end
+    def img(el)
+      "<img src='#{h el.attributes['src']}' alt='#{h el.attributes['alt'] || ''}'>"
+    end
 
-  def h(str)
-    CGI.escapeHTML str.to_s
-  end
+    def h(str)
+      CGI.escapeHTML str.to_s
+    end
 
-  def check_updated(token)
-    throw :not_updated if feed.data.last_token == token
+    def check_updated(token)
+      throw :not_updated if feed.data.last_token == token
+    end
   end
 end
--- lib/scrape/feed.rb
+++ lib/scrape/feed.rb
@@ -2,9 +2,10 @@ require 'ostruct'
 
 URL = "http://scrape.heroku.com/"
 
-class Feed < Struct.new(:name, :url, :opts, :block, :data)
-  def render
-    Haml::Engine.new(<<HAML).render(self)
+module Scrape
+  class Feed < Struct.new(:name, :url, :opts, :block, :data)
+    def render
+      Haml::Engine.new(<<HAML).render(self)
 !!! XML
 %feed{:xmlns => "http://www.w3.org/2005/Atom", 'xml:base' => url}
   %title&= name.titleize
@@ -14,10 +15,11 @@ class Feed < Struct.new(:name, :url, :opts, :block, :data)
 
   = entry.render
 HAML
-  end
+    end
 
-  def entry
-    return @entry if @entry && Time.now - @entry.created_at < 1.hour
-    @entry = Entry.new(self)
+    def entry
+      return @entry if @entry && Time.now - @entry.created_at < 1.hour
+      @entry = Entry.new(self)
+    end
   end
 end
